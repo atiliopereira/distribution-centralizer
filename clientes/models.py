a47085906fc9f473897ba5c1104436b8af55fc63
chilu <atilio@dissoi.com>
2020-07-31 18:28:00 -0400

    VERTI-8 (cliente) eliminar grupo y modificar clase cliente

--- clientes/models.py
+++ clientes/models.py
@@ -3,24 +3,28 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
-class Grupo(models.Model):
-    nombre = models.CharField(max_length=100)
-    dia_de_presentacion = models.IntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(30)],
-                                              verbose_name="día de presentación de facturas")
-
-    def __str__(self):
-        return self.nombre
-
-
 class Cliente(models.Model):
-    grupo = models.ForeignKey(Grupo, null=True, blank=True, on_delete=models.PROTECT)
-    razon_social = models.CharField(max_length=100, verbose_name="razón social")
+    razon_social = models.CharField(max_length=150, verbose_name="razón social")
     ruc = models.CharField(max_length=20, verbose_name="RUC", unique=True)
-    direccion = models.CharField(max_length=200, blank=True, verbose_name="dirección")
+    direccion = models.CharField(max_length=200, blank=True, verbose_name="dirección de facturación")
     telefono = models.CharField(max_length=50, blank=True, verbose_name="teléfono")
     email = models.CharField(max_length=100, null=True, blank=True, verbose_name="e-mail")
+    dia_de_presentacion = models.IntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(30)],
+                                              verbose_name="día de presentación de facturas")
     activo = models.BooleanField(default=True)
 
     def __str__(self):
         return self.razon_social
 
+
+class PuntoEntregaCliente(models.Model):
+    class Meta:
+        verbose_name = "Punto de entrega"
+        verbose_name_plural = "Puntos de entrega"
+
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    referencia = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=200, blank=True, verbose_name="dirección")
+
+    def __str__(self):
+        return f'{self.cliente}: {self.referencia}'
