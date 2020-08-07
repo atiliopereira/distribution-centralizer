6b4c10937071f7f17e27c8b963313234632019ed
chilu <atilio@dissoi.com>
2020-08-07 16:33:37 -0400

    VERTI-19 (lista de ventas) agregar filtros de busqueda y acceso a reportes

--- ventas/admin.py
+++ ventas/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.utils.safestring import mark_safe
 
 from sistema.constants import EstadoDocumento
+from sistema.globales import separar
 from ventas.forms import VentaSearchForm
 from ventas.models import DetalleDeVenta, Venta
 from ventas.views import get_ventas_queryset
@@ -34,6 +35,11 @@ class VentaAdmin(admin.ModelAdmin):
     def has_delete_permission(self, request, obj=None):
         return False
 
+    def lookup_allowed(self, lookup, *args, **kwargs):
+        if lookup in self.advanced_search_form.fields.keys():
+            return True
+        return super(VentaAdmin, self).lookup_allowed(lookup, *args, **kwargs)
+
     def get_queryset(self, request):
         form = self.advanced_search_form
         qs = get_ventas_queryset(request, form)
@@ -46,8 +52,14 @@ class VentaAdmin(admin.ModelAdmin):
         self.other_search_fields = {}
         params = request.get_full_path().split('?')
 
+        ventas_queryset = self.get_queryset(request)
+        total_ventas = 0
+        for venta in ventas_queryset:
+            total_ventas += venta.total
+
         extra_context = extra_context or {}
         extra_context.update({'asf': VentaSearchForm,
+                              'total': separar(int(round(total_ventas))),
                               'my_request_get': self.my_request_get,
                               'params': '?%s' % params[1].replace('%2F', '/') if len(params) > 1 else ''
                               })
