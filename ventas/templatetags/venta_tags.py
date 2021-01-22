8e2e6649b3509ea3c82c56780c110936cfceadd0
chilu <atilio@dissoi.com>
2021-01-22 14:35:40 -0300

    VERTI-45 panel de administracion

--- ventas/templatetags/venta_tags.py
+++ ventas/templatetags/venta_tags.py
@@ -9,3 +9,9 @@ register = template.Library()
 @register.inclusion_tag('admin/ventas/venta/venta_search_form.html', takes_context=True)
 def venta_search_form(context, cl):
     return advanced_search_form(context, cl)
+
+
+@register.filter
+def eliminar_separador_miles(numero):
+    numero_str = str(numero)
+    return numero_str
